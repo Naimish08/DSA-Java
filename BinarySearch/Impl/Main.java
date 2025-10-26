79300264c32bd8e2a95152dd7ab753ce18725839
Naimish Shah <naimishshah8@gmail.com>
2025-10-26 18:09:11 +0530

    New More Code Added

---
+++ BinarySearch/Impl/Main.java
@@ -0,0 +1,8 @@
+package BinarySearch.Impl;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.search(new int[]{1,2,3,4,5,6},6));
+    }
+}
